509736795ee77c03e52428a88e1aebd76f5f8122
Valentina Perona <2112356@ucc.edu.ar>
2026-02-10 14:47:07 -0300

    fix(frontend-prod): point apiUrl to current production backend

--- tf-frontend/src/environments/environment.prod.ts
+++ tf-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://userapi-737714447258.us-central1.run.app/api/v1'
+  apiUrl: 'https://userapi-753295178829.us-central1.run.app/api/v1'
 };
