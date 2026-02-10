ba0cbbb645385c36ea84da7474e3414af974f9cd
Valentina Perona <2112356@ucc.edu.ar>
2026-02-10 14:38:07 -0300

    fix(frontend-qa): point apiUrl to current userapi-qa service

--- tf-frontend/src/environments/environment.qa.ts
+++ tf-frontend/src/environments/environment.qa.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // Esta URL se actualizará una vez que se cree el servicio userapi-qa
-  apiUrl: 'https://userapi-qa-737714447258.us-central1.run.app/api/v1'
+  apiUrl: 'https://userapi-qa-753295178829.us-central1.run.app/api/v1'
 };
